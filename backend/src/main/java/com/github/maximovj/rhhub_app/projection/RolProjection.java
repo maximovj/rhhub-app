f75a5bc558decbddd8e572fa36a067bc8535ffd0
Víctor J. <maximovj@outlook.com>
2026-02-12 11:10:25 -0600

    * Se corrige projection para Rol

--- backend/src/main/java/com/github/maximovj/rhhub_app/projection/RolProjection.java
+++ backend/src/main/java/com/github/maximovj/rhhub_app/projection/RolProjection.java
@@ -5,6 +5,9 @@ import com.github.maximovj.rhhub_app.entity.RolEntity;
 
 public interface RolProjection {
 
+    @JsonProperty("rol_id")
+    Long getRolId();
+
     @JsonProperty("nombre")
     String getNombre();
     
@@ -13,6 +16,7 @@ public interface RolProjection {
 
     public static RolProjection fromEntity(RolEntity e) {
         return new RolProjection() {
+            public Long getRolId() { return e.getRolId(); }
             public String getNombre() { return e.getNombre(); }
             public String getDescripcion() { return e.getDescripcion(); }
         };
